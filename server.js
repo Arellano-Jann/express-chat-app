b369131e4caec8560961da4534571ef3aea413f8
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-03-22 18:18:35 -0700

    fully added joining and leaving logic

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser }= require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers }= require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,21 +15,34 @@ const botName = 'Bot';
 io.on('connection', (socket) => { // runs when client connects
     console.log('New WS connection...');
     socket.on('joinRoom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room); // socket joins a room that the user has specified. the socket can be thought of as a "user" but abstracted
+
         socket.emit('message', formatMessage(botName, 'Welcome to the chat app!')); // emits an event to the client/user
-        socket.broadcast.emit('message', formatMessage(botName, 'A user has fucking joined.')); // emits an event to all clients/users except the one that is connected
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has fucking joined.`)); // emits an event to all clients/users IN A SPECIFIC ROOM/PLACE except the one that is connected
+        // socket.broadcast.emit('message', formatMessage(botName, 'A user has fucking joined.')); // emits an event to all clients/users except the one that is connected
         // io.emit(); // emits an event to all clients/users
+
+        
+        
     });
     
-
-    
-    socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+    // he put this outside of the join room for some reason idk why. it might break if this is inside of the joinRoom somehow but we'll see idk. it might also be because of the "on" event listener
+    socket.on('chatMessage', (msg) => { // listens for a chatMessage event from main.js that comes from the page
+        const user = getCurrentUser(socket.id); // gets the user connected to the socket
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
     
     socket.on('disconnect', () => { // listens for a disconnect event
-        io.emit('message', formatMessage(botName, 'A user has left the fucking chat.')); // to run an emit notifying all users
-        console.log('Closing WS connection...')
+        const user = userLeave(socket.id); // gets the user connected to the socket
+
+        if (user){ // if user exists. don't really know why this is tbh but eh.
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has fucking left.`)); // to run an emit notifying all users
+            console.log('Closing WS connection...')
+        }
+
     })
+
 });
 
 const PORT = 3000 || process.env.PORT; // uses 3000 as a default port unless there exists a preconfigured port environment variable
--- utils/users.js
+++ utils/users.js
@@ -12,7 +12,25 @@ function getCurrentUser(id){
     return users.find(user => user.id === id);
 };
 
+function userLeave(id) { // user leaves the chat
+    const i = users.findIndex(user => user.id === id); // finds the index of the user in the users array
+    // const i = users.findIndex((user) => {
+    //     user.id === id; // triple equals is used to compare the value and the type? idk
+    // });
+    if (i !== -1) {
+        return users.splice(i, 1)[0]; // returns the removed user
+    }
+}
+
+function getRoomUsers(room){
+    return users.filter((user) => {
+        user.room === room; // triple equals is used to compare the value and the type? idk
+    });
+}
+
 module.exports = {
     userJoin,
-    getCurrentUser
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
 };
